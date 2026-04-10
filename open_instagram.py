ba63d15cc59e96f3a26ba59650f30b26541bf110
Mehmet Efe İLİMAN <efe2007iliman@gmail.com>
2026-04-10 23:28:46 +0300

    Fundamentals

---
+++ open_instagram.py
@@ -0,0 +1,59 @@
+from playwright.sync_api import sync_playwright
+import json
+
+
+def generate_session():
+    with sync_playwright() as p:
+        
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+        
+        with open('session.json', 'r') as file:
+            data = json.load(file)
+        cookies = data["cookies"]
+        
+        context.add_cookies(cookies)
+
+        print("Navigating to Instagram...")
+        page.goto("https://www.instagram.com/your_activity/interactions/likes/")
+        
+        isCompleted = False
+        while(True):
+            page.get_by_text("Select").click()
+            button = page.get_by_label("Image with button")
+
+            if button.count()==0:
+                print("No posts found!")
+                isCompleted = True
+                break
+            
+            i=0
+            while i<100:
+                if i>button.count()-1:
+                    print("No more posts to unlike!")
+                    isCompleted=True
+                    break
+                                
+                button.nth(i).click()
+                i+=1
+                page.wait_for_timeout(200)
+            
+            page.get_by_text("Unlike").click()
+            page.get_by_role("button").filter(has_text="Unlike").click()
+            
+            if isCompleted is True:
+                break
+        
+            page.reload()
+            page.wait_for_timeout(5000)
+        
+        
+        print("=================================================================")
+        print("Progress Done!")        
+        
+        browser.close()
+        p.stop()
+
+if __name__ == "__main__":
+    generate_session()
---
+++ save_state.py
@@ -0,0 +1,25 @@
+from playwright.sync_api import sync_playwright
+
+def generate_session():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
+
+        print("Navigating to Instagram...")
+        page.goto("https://www.instagram.com/")
+        
+        print("\n*** ACTION REQUIRED ***")
+        print("1. Log into your account in the browser window.")
+        print("2. Handle any 2FA or 'Save Info' prompts.")
+        input("3. Once you can clearly see your feed, press ENTER in this terminal to continue... ")
+
+        print("Saving session data...")
+        context.storage_state(path="session.json")
+        
+        print("Success! Session saved as 'session.json'.")
+        
+        browser.close()
+
+if __name__ == "__main__":
+    generate_session()
